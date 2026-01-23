3754c715a2b22060202893a54a5115c5ee038694
Ren-mayday <rencel.dayrit@gmail.com>
2026-01-23 16:09:05 +0100

    added correct ENDPOINT to server.js. Tested executionwith latest version of scrapper.js (max 15 scrolls) and longer time. It does work and created the product.json file successfully. With these updates extraction does not freeze.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { connectDB } = require("./src/config/db");
 const earingsRoutes = require("./src/api/routes/earingsRoutes");
+const { deleteEaring } = require("./src/api/controllers/earingsControllers");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,7 +21,11 @@ app.get("/", (req, res) => {
   res.json({
     message: "API de Pendientes (productos de San Saru) funcionando correctamente",
     endpoints: {
-      earings: "/api/earings",
+      getAllEarings: "GET /api/v1/earings",
+      getEaringById: "GET /api/v1/earings/:id",
+      createEaring: "POST /api/v1/earings",
+      updateEaring: "PUT /api/v1/earings/:id",
+      deleteEaring: "DELETE /api/v1/earings/:id",
     },
   });
 });
